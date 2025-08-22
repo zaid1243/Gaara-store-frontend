0406058f35b38b2ee014a940e2deab6d2d56eea4
zaid <skzaid8821@gmail.com>
2025-08-22 17:52:46 +0530

    changed

--- src/modals/Auth.jsx
+++ src/modals/Auth.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { useAppContext } from "../context/appContext";
+import { useAppContext } from "../context/AppContext";
 import { toast } from "react-hot-toast";
 const Auth = () => {
   const [state, setState] = useState("login");
